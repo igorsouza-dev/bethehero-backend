34eb21b49e0a75b655db2ad42c3bb5a33b71be60
Igor Souza <igorsouza.dev@gmail.com>
2020-03-24 15:23:22 -0300

    Added migrations. Added knex integration. Added routes file

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Be The Hero - Backend
+
+### Knex CLI commands
+
+```sh
+# create migration
+yarn knex migrate:make migration_name
+
+# run migrations
+yarn knex migrate:latest
+```
--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import routes from './routes';
 
 const app = express();
-app.get('/', (req, res) => res.send('hello world'));
+
 app.use(express.json());
 app.use(cors());
+
+app.use(routes);
 
 app.listen(3333);
