81404c387e8ff4ea52b72d78ae139e3c053ce298
Igor Souza <igorsouza.dev@gmail.com>
2020-03-24 10:20:26 -0300

    Added cors and json support

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 app.get('/', (req, res) => res.send('hello world'));
+app.use(express.json());
+app.use(cors());
+
 app.listen(3333);
